fcb08727a71ce5d83b7ea52b1e7b67ae923acf04
sfoolish <sfoolish.liang@gmail.com>
2017-10-01 22:04:36 +0800

    update
    Signed-off-by: sfoolish <sfoolish.liang@gmail.com>

--- 5_Go/test-code/src/7.goroutine_chan.go
+++ 5_Go/test-code/src/7.goroutine_chan.go
@@ -1,4 +1,4 @@
-package "main"
+package main
 
 import (
 	"exec"
@@ -9,38 +9,37 @@ import (
 // https://www.johnsto.co.uk/blog/function-timeouts-in-go/
 
 func CmdRunWithTimeout(cmd *exec.Cmd, timeout time.Duration) (error, bool) {
-    done := make(chan error)
-    go func() {
-        done <- cmd.Wait()
-    }()
+	done := make(chan error)
+	go func() {
+		done <- cmd.Wait()
+	}()
 
-    var err error
-    select {
-    case <-time.After(timeout):
-        // timeout
-        if err = cmd.Process.Kill(); err != nil {
-            log.Error("failed to kill: %s, error: %s", cmd.Path, err)
-        }
-        go func() {
-            <-done // allow goroutine to exit
-        }()
-        log.Info("process:%s killed", cmd.Path)
-        return err, true
-    case err = <-done:
-        return err, false
-    }
+	var err error
+	select {
+	case <-time.After(timeout):
+		// timeout
+		if err = cmd.Process.Kill(); err != nil {
+			log.Error("failed to kill: %s, error: %s", cmd.Path, err)
+		}
+		go func() {
+			<-done // allow goroutine to exit
+		}()
+		log.Info("process:%s killed", cmd.Path)
+		return err, true
+	case err = <-done:
+		return err, false
+	}
 }
 
 func main() {
 
-cmd := exec.Command(realRun)
-var stdout bytes.Buffer
-cmd.Stdout = &stdout
-var stderr bytes.Buffer
-cmd.Stderr = &stderr
-cmd.Start() // attention!
-
-var isTimeout bool
-err, isTimeout = systool.CmdRunWithTimeout(cmd, time.Duration(t)*time.Second)
+	cmd := exec.Command(realRun)
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	cmd.Start() // attention!
+	var isTimeout bool
+	err, isTimeout = systool.CmdRunWithTimeout(cmd, time.Duration(t)*time.Second)
 
 }
